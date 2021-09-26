73ef57e6bb9b7727516b3b0511882d9307d36378
QuadnucYard <2380433991@qq.com>
2021-09-26 08:37:29 +0800

    增加波组成推算

--- exscore.js
+++ exscore.js
@@ -5,6 +5,9 @@ Array.prototype.groupBy = function (iteratee, context) { return _.groupBy(this, 
 Array.prototype.sortBy = function (iteratee, context) { return _.sortBy(this, iteratee, context); };
 Array.prototype.uniq = function (isSorted, iteratee) { return _.uniq(this, isSorted, iteratee); };
 Array.prototype.maps = function (callbackfn, sep = "") { return this.map(callbackfn).join(sep); };
+Array.prototype.sum = function (pred = t => t, initial = 0) {
+    return this.reduce((prev, cur) => prev + pred(cur), initial);
+}
 
 String.prototype.takeFrom = function (pattern, offset = 0) { return this.substr(this.indexOf(pattern) + offset); };
 String.prototype.last = function () { return this.charAt(this.length - 1); };
