567303c4a5e0c7fda48b6afa2f26427c383e8cf1
QuadnucYard <2380433991@qq.com>
2021-09-29 20:12:40 +0800

    Add some functions

--- exscore.js
+++ exscore.js
@@ -1,5 +1,5 @@
 
-_.prototype.maps = function (list, iteratee, sep = "") { return _.map(list, iteratee).join(sep); }
+_.maps = function (list, iteratee, sep = "") { return _.map(list, iteratee).join(sep); }
 
 Array.prototype.groupBy = function (iteratee, context) { return _.groupBy(this, iteratee, context); };
 Array.prototype.sortBy = function (iteratee, context) { return _.sortBy(this, iteratee, context); };
@@ -7,7 +7,23 @@ Array.prototype.uniq = function (isSorted, iteratee) { return _.uniq(this, isSor
 Array.prototype.maps = function (callbackfn, sep = "") { return this.map(callbackfn).join(sep); };
 Array.prototype.sum = function (pred = t => t, initial = 0) {
     return this.reduce((prev, cur) => prev + pred(cur), initial);
-}
+};
 
 String.prototype.takeFrom = function (pattern, offset = 0) { return this.substr(this.indexOf(pattern) + offset); };
 String.prototype.last = function () { return this.charAt(this.length - 1); };
+
+_.partialSum = function (arr) {
+    let ret = [...arr];
+    for (let i = 1; i < ret.length; i++) {
+        ret[i] += ret[i - 1];
+    }
+    return ret;
+};
+
+_.adjacentDifference = function (arr) {
+    return arr.map((t, i) => i == 0 ? t : t - arr[i - 1]);
+};
+
+_.innerProduct = function (arr, brr) {
+    return arr.reduce((prev, cur, i) => prev + cur * brr[i], 0)
+}
