c18d368ab9137faacf4f40b763b78a85283ec380
QuadnucYard <2380433991@qq.com>
2021-09-29 09:09:35 +0800

    优化小数格式化

--- format.js
+++ format.js
@@ -8,6 +8,12 @@ window.WebFontConfig = {
     },
 };
 
+function toApprecision(num, digits) {
+    let s1 = num.toString();
+    let i = s1.indexOf('.');
+    return i == -1 || s1.length - i - 1 <= digits ? s1 : num.toFixed(digits);
+}
+
 function formatHtml(_str, _color = 0, _b = false, _size = 14, _font = "") {
     return `<span style="color:#${_color.toString(16).padStart(6, '0')};${_b ? "font-weight:bold;" : ""}${_size ? `font-size:${_size};` : ""}${_font ? `font-family:${_font};` : ""}">${_str.replaceAll("\n", "<br>")}</span>`;
 }
